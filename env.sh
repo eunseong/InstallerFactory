a7e9cb0939fe114e51ca4973311d9575795cf781
root <root@pl-8-builder-1.tk>
2021-03-31 13:14:35 +0900

    change dir struct

--- env.sh
+++
@@ -1,4 +0,0 @@
-FD=/vagrant/InstallerFactory
-DIST="8" #"8.2"
-COMMIT=3
-WD=$FD/prolinux-$DIST
--- setup.sh
+++
@@ -1,1 +0,0 @@
-https://app.vagrantup.com/eslee/boxes/prolinux8_anaconda/versions/8.2.210330/providers/virtualbox.box
