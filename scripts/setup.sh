bf185ca5099dfbb545a16979494d8138dc94b047
root <root@pl-8-builder-1.tk>
2021-03-31 13:27:57 +0900

    update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,9 @@
-https://app.vagrantup.com/eslee/boxes/prolinux8_anaconda/versions/8.2.210330/providers/virtualbox.box
+#!/bin/bash
+
+#dnf install -y 
+
+wget -P /tmp https://app.vagrantup.com/eslee/boxes/prolinux8_anaconda/versions/8.2.210330/providers/virtualbox.box \
+	&& vagrant box add prolinux8/installer_gen /tmp/virtualbox.box \
+	&& rm -f /tmp/virtualbox.box 
+
+vagrant up
