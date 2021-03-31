f0070d9abbd830cee2caf210de88c0bfc0243dde
root <root@pl-8-builder-1.tk>
2021-03-31 20:22:32 +0900

    trivial

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,5 +4,5 @@
 
 wget -P /tmp https://app.vagrantup.com/eslee/boxes/prolinux8_anaconda/versions/8.2.210330/providers/virtualbox.box \
 	&& vagrant box add prolinux8/installer_gen /tmp/virtualbox.box \
-	&& rm -f /tmp/virtualbox.box \ 
+	&& rm -f /tmp/virtualbox.box \
 	&& vagrant up
