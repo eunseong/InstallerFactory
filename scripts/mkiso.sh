c7323b6bb3dcdfd30280b1ed0e69788c5bfd1267
root <root@pl-8-builder-1.tk>
2021-03-31 17:07:14 +0900

    error checking

--- scripts/mkiso.sh
+++ scripts/mkiso.sh
@@ -18,7 +18,7 @@ if [ -e $OUT/$OUT_NAME ]; then
 	read -p "Original ISO file '$OUT_NAME' already exist. Remove this? [y/n]: " res
 	[ "$res" != "y" ] && [ "$res" != "Y" ] && exit 1
 	echo "remove the file~"
-	rm -f $OUT/$OUT_NAME	
+	rm -f $OUT/$OUT_NAME
 fi
 
 echo -e "\n(1/5)Download and install the Installer meta files\n"
@@ -40,7 +40,8 @@ if [ -d $WD/AppStream/ ]; then
 fi
 if [[ ! -d $REPO/BaseOS || ! -d $REPO/AppStream ]]; then
 	echo -e "\n\nPrepare ProLinux package first!, end the image generation"
-	echo -e "Repo download guide: reposync --repoid=pl-baseos"
+	echo -e "\tRepo download guide: reposync --repoid=pl8-BaseOS --downloaddir=$REPO --download-metadata"
+	echo -e "\t                     reposync --repoid=pl8-AppStream --downloaddir=$REPO --download-metadata"
 	exit 1
 fi
 cp -rT $REPO/BaseOS $WD/BaseOS 
@@ -57,16 +58,16 @@ if [ -d $WD/AppStream/repodata ]; then
 	echo remove AppStream repo-meta directory
 	rm -rf $WD/AppStream/repodata $WD/AppStream/.repodata
 fi
-createrepo_c -g $FD/repo/comps-BaseOS.x86_64.xml $WD/BaseOS/
-createrepo_c -g $FD/repo/comps-AppStream.x86_64.xml $WD/AppStream/
-modifyrepo_c $FD/repo/modules.yaml $WD/AppStream/repodata/
+createrepo_c -g $REPO/BaseOS/repodata/*comps-BaseOS.x86_64.xml $WD/BaseOS/
+createrepo_c -g $REPO/AppStream/repodata/*comps-AppStream.x86_64.xml $WD/AppStream/
+gzip -d $REPO/AppStream/repodata/*modules.yaml.gz
+modifyrepo_c $REPO/AppStream/repodata/*modules.yaml $WD/AppStream/repodata/
 
 
 echo -e "\n(4/5)Create Installer ISO\n"
 mkisofs -U -r -v -T -J -joliet-long -allow-limited-size -V "ProLinux-"$DIST" Server.x86_64" -volset "ProLinux-"$DIST" Server.x86_64" \
 			-b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
-			-boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot -o $OUT/$OUT_NAME \
-			$WD/
+			-boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot -o $OUT/$OUT_NAME $WD/
 
 
 echo -e "\n(5/5)Implant&Check md5 checksum in the ISO\n"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#dnf install -y 
+#dnf install kernel-devel vagrant VirtualBox 
 
 wget -P /tmp https://app.vagrantup.com/eslee/boxes/prolinux8_anaconda/versions/8.2.210330/providers/virtualbox.box \
 	&& vagrant box add prolinux8/installer_gen /tmp/virtualbox.box \
 	&& rm -f /tmp/virtualbox.box 
-
-vagrant up
+	&& vagrant up
