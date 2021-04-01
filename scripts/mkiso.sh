175e73808cdd79a66a0bf10e305088d237191920
eunseong <godpeople16@naver.com>
2021-04-01 18:20:24 +0900

    Update mkiso.sh

--- scripts/mkiso.sh
+++ scripts/mkiso.sh
@@ -41,7 +41,7 @@ fi
 if [[ ! -d $REPO/BaseOS || ! -d $REPO/AppStream ]]; then
 	echo -e "\n\nPrepare ProLinux package first!, end the image generation"
 	echo -e "\tRepo download guide: reposync --repoid=pl8-BaseOS --downloaddir=$REPO --download-metadata"
-	echo -e "\t                     reposync --repoid=pl8-AppStream --downloaddir=$REPO --download-metadata"
+	echo -e "\t\t\t\t\t reposync --repoid=pl8-AppStream --downloaddir=$REPO --download-metadata"
 	exit 1
 fi
 cp -rT $REPO/BaseOS $WD/BaseOS 
